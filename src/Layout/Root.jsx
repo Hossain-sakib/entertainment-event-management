bbf55021cfbcd7a89bcc88b8e85f12d07d8f0cae
Hossain-sakib <mskib567@gmail.com>
2023-10-09 14:47:19 +0600

    fixed the responsive issue

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -5,7 +5,7 @@ import Footer from "../Shared/Footer/Footer";
 
 const Root = () => {
     return (
-        <div className="max-w-7xl mx-auto ">
+        <div className="max-w-7xl mx-auto px-2">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
